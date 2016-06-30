15e3498b008d4031d0df2806f01f1cc98e6dc9d1
dsuare1 <suarez.derrick@gmail.com>
2016-06-30 16:39:41 -0500

    tweaking scroll and modal width

--- assets/css/freelancer/js/cbpAnimatedHeader.js
+++ assets/css/freelancer/js/cbpAnimatedHeader.js
@@ -1,25 +1,15 @@
-/**
- * cbpAnimatedHeader.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
 var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 200;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
-				setTimeout( scrollPage, 250 );
+				setTimeout( scrollPage, 100 );
 			}
 		}, false );
 	}
