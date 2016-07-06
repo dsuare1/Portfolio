5a1f5be5527e73ff396b8567ba2106ae65a640c2
dsuare1 <suarez.derrick@gmail.com>
2016-07-05 20:14:12 -0500

    added text to developer section

---
+++ assets/js/mail-server.js
@@ -0,0 +1,11 @@
+// steps for setting up mail capabilities on portfolio site:
+
+	// 1) require (npm 'mailgun')
+
+	// 2) start up Express server
+
+	// 3) create endpoint that runs mailgun code
+
+	// 4) on form submit, do a 'POST' request to endpoint
+
+	// 	$.POST('/formSubmit', etc.)
